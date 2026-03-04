9f8daa840254df2d360a46f6ce9138ed4bcb4caa
Guilherme Carvalho <guilhefcarvalho@hotmail.com>
2026-03-04 23:32:15 +0000

    More mlflow changes

--- src/mlflow_functions.py
+++ src/mlflow_functions.py
@@ -1,19 +1,20 @@
-import subprocess
-import time
-import webbrowser
 import mlflow
+import subprocess
+import webbrowser
+import time
 
+def start_mlflow(experiment_name, open_ui=True):
 
-def start_mlflow(experiment_name):
-    subprocess.Popen(
-        ['mlflow', 'ui', '--backend-store-uri', 'file:Tracking'],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
-
-    time.sleep(3)
-
-    mlflow.set_tracking_uri("http://localhost:5000")
+    mlflow.set_tracking_uri("file:./Tracking")
+    #Fil based tracking
     mlflow.set_experiment(experiment_name)
-    webbrowser.open("http://localhost:5000")
     
+    # Optionally start UI
+    if open_ui:
+        subprocess.Popen(
+            ['mlflow', 'ui', '--backend-store-uri', 'file:Tracking'],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+        time.sleep(3)
+        webbrowser.open("http://localhost:5000")
